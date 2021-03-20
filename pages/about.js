2feed4fe7ca3b2c227f0b4b7ec7cc4e4d971f70f
shivanishimpi <shivanishimpi9@gmail.com>
2021-03-20 19:43:33 +0530

    📍resumeUpdate

--- pages/about.js
+++ pages/about.js
@@ -14,7 +14,7 @@ export default function About() {
 					description,
 				}}
 			/>
-			<div className="flex flex-col justify-center items-start max-w-1xl mx-auto mb-16">
+			<div className="flex flex-col justify-left items-start max-w-1xl mx-auto mb-16">
 				<h2 className="font-bold justify-left text-2xl md:text-5xl tracking-tight mb-4 dark:text-white">Ritik Sharma</h2>
 				<p className="text-2xl text-gray-600 dark:text-gray-400">
 					A Full Stack Developer currently looking for new a challenge
