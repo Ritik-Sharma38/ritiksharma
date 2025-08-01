09b42b6319c4f3095527b2d54dda2774cc407d03
Ritik Sharma <rd@ritiksharma.in>
2025-08-01 20:24:53 +0530

    Create friend.js

---
+++ pages/friend.js
@@ -0,0 +1,13 @@
+export default function Friend() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-white text-center px-4">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">
+        Hey! You scanned the code 🎉
+      </h1>
+      <p className="text-lg text-gray-600">
+        You’re a good friend! You own a pizza on me! 🍕<br />
+        Send me this screenshot 😄
+      </p>
+    </div>
+  );
+}
