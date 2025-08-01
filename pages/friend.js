0c97256bd7843e84f7aa385b006becdb3f786991
Ritik Sharma <rd@ritiksharma.in>
2025-08-01 20:37:03 +0530

    Update friend.js

--- pages/friend.js
+++ pages/friend.js
@@ -1,10 +1,10 @@
 export default function Friend() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-white text-center px-4">
-      <h1 className="text-4xl font-bold text-gray-800 mb-4">
-        Hey! You scanned the code 🎉
+    <div className="min-h-screen flex flex-col items-center justify-center bg-white dark:bg-black text-center px-4">
+      <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-100 mb-4">
+        Hey! You scanned the QR code 🎉
       </h1>
-      <p className="text-lg text-gray-600">
+      <p className="text-lg text-gray-600 dark:text-gray-300">
         You’re a good friend! You own a pizza on me! 🍕<br />
         Send me this screenshot 😄
       </p>
